30ba971be8b93b8cd4d1fda995757fa923404c53
Erin Kelley <erin.kelley01@gmail.com>
2024-09-09 14:36:19 -0600

    Add image and title to header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<header>
+    <div class="flex bg-custom-blue">
+        <img
+            class="h-60"
+            src="cat.jpeg"
+            alt="Orange cat with round glasses on"
+        />
+        <h1>Cat Corner</h1>
+    </div>
+</header>
