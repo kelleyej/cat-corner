9a04899dc6af8712471e78d37b132e3162374803
Erin Kelley <erin.kelley01@gmail.com>
2024-09-09 15:36:48 -0600

    Fetch cat breeds on initial mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
     import "../app.css";
     import Header from "$lib/Header.svelte";
+    import { onMount } from "svelte";
+    import { breedStore } from "../store.js";
+
+    onMount(async () => {
+        fetch("https://api.thecatapi.com/v1/breeds")
+            .then((response) => response.json())
+            .then((data) => breedStore.set(data));
+    });
 </script>
 
 <Header />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { breedStore } from "../store.js";
+    let catBreeds = [];
+
+    $: {
+        catBreeds = $breedStore;
+        console.log(catBreeds);
+    }
+</script>
