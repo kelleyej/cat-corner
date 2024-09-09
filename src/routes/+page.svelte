15bd36d5ef594feeadff453c443886333486e134
Erin Kelley <erin.kelley01@gmail.com>
2024-09-09 15:46:52 -0600

    Add cat name, temperament, and description to display grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,13 @@
         console.log(catBreeds);
     }
 </script>
+
+<section class="grid grid-cols-3 gap-5 text-center">
+    {#each catBreeds as cat (cat.id)}
+        <div>
+            <h2 class="font-bold">{cat.name}</h2>
+            <h3>{cat.temperament}</h3>
+            <p>{cat.description}</p>
+        </div>
+    {/each}
+</section>
