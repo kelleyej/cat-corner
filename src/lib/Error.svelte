4be65a8d289321a5ece0e4de82f3bf5dafbacd10
Erin Kelley <erin.kelley01@gmail.com>
2024-09-30 09:07:55 -0600

    Add a go home button

--- src/lib/Error.svelte
+++ src/lib/Error.svelte
@@ -1,12 +1,21 @@
-<header class="flex h-80 bg-custom-purple border-b-4 border-green-600">
-    <div class="w-8/12 h-80 relative border-b-4 border-green-600">
-        <img class="absolute bottom-0" src="./sad-cat.jpeg" alt="Sad cat" />
-    </div>
+<script>
+</script>
 
-    <!-- <div class="w-4/12 h-72 flex items-center">
+<body class="bg-blue-100 h-screen">
+    <header class="flex h-80 bg-custom-purple border-b-4 border-green-600">
+        <div class="w-8/12 h-80 relative border-b-4 border-green-600">
+            <img class="absolute bottom-0" src="./sad-cat.jpeg" alt="Sad cat" />
+        </div>
+
+        <!-- <div class="w-4/12 h-72 flex items-center">
        
     </div> -->
-</header>
-<h1 class="text-4xl text-center">
-    Oh no! Looks like there is a small cat-astrophe. That page does not exist.
-</h1>
+    </header>
+    <section class="mt-5 flex items-center flex-col">
+        <h1 class="text-7xl text-center pr-5 pl-5">
+            Oh no! Looks like there is a small cat-astrophe. That page does not
+            exist.
+        </h1>
+        <button class="mt-10 border-2">Go Home</button>
+    </section>
+</body>
