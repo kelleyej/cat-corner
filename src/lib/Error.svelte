fc3e500e66dc85784c65acb47365cd73c71b58f3
Erin Kelley <erin.kelley01@gmail.com>
2024-09-30 09:01:35 -0600

    Add image to error page header

--- src/lib/Error.svelte
+++ src/lib/Error.svelte
@@ -1,1 +1,12 @@
-<p>THIS IS AN ERROR YAY</p>
+<header class="flex h-80 bg-custom-purple border-b-4 border-green-600">
+    <div class="w-8/12 h-80 relative border-b-4 border-green-600">
+        <img class="absolute bottom-0" src="./sad-cat.jpeg" alt="Sad cat" />
+    </div>
+
+    <!-- <div class="w-4/12 h-72 flex items-center">
+       
+    </div> -->
+</header>
+<h1 class="text-4xl text-center">
+    Oh no! Looks like there is a small cat-astrophe. That page does not exist.
+</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
         'custom-blue': '#D0F9FF',
         'custom-beige': '#E3FAE3',
         'custom-brown': '#FFFAF4',
-        'custom-bronze': '#CD7F2D'
+        'custom-bronze': '#CD7F2D',
+        'custom-purple': '#DBCFE3'
       },
       fontFamily: {
         'catamaran': 'Catamaran',
